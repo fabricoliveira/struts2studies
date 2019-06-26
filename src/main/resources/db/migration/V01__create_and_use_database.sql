4f19b123971340674b88e95026f8493a5f4df96e
fabricoliveira <fabricoliveira@192.168.0.104>
2019-06-25 21:28:06 -0300

    database

--- src/main/resources/db/migration/V01__create_and_use_database.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS products;
-CREATE DATABASE products;
-USE products;
--- src/main/resources/db/migration/V02__create_table_and_populate_login.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE login (
-	username VARCHAR(30) UNIQUE,
-	password VARCHAR(30)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO login (username, password) VALUES ('admin', 'blablabla');
